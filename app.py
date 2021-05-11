89dd9afb0dd5d29ad1e169cf61197784f34b66e5
p1nea77le <p1nea77le@gmail.com>
2021-05-11 16:57:50 +0900

    WebApp5/11

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '''
+    <!DOCTYPE html>
+    <html>
+    <body>
+
+    <h1>웹 앱 프로그래밍</h1>
+
+    <p><a href="http://127.0.0.1:5000/hello">헬로 페이지</a></p>
+
+    <p><a href="http://127.0.0.1:5000/naver">네이버 페이지</a></p>
+
+    </body>
+    </html>
+
+    '''
+
+@app.route('/hello')
+def hello():
+    return 'hello'    
+
+@app.route('/naver')
+def naver():
+    return '네이버'    
+
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ game.py
@@ -0,0 +1,3 @@
+import func
+print("내가 만든 게임 시작")
+func.dora()
