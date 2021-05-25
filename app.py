eb6aee90a8d4ec908adbad64e463170dca0d11ae
p1nea77le <p1nea77le@gmail.com>
2021-05-25 17:18:11 +0900

    5/26

--- app.py
+++ app.py
@@ -1,41 +1,39 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+
 @app.route('/')
-def index():
-    return '''
-    <!DOCTYPE html>
-    <html>
-    <body>
-
-    <h1>웹 앱 프로그래밍</h1>
-
-    <p><a href="http://127.0.0.1:5000/hello">헬로 페이지</a></p>
-
-    <p><a href="http://127.0.0.1:5000/naver">네이버 페이지</a></p>
-
-    </body>
-    </html>
-
-    '''
-
-@app.route('/hello')
 def hello():
     return render_template("main.html")  
 
-@app.route('/naver')
-def naver():
-    return render_template("naver.html")   
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html")
+    else:
+        # POST로 들어오는 데이터를 받아보자
+        email = request.form['email']
+        pwd = request.form['pwd']
+        print("전달된 값:" ,email, pwd)
 
-@app.route('/search', methods=['GET', 'POST'])
-def search(): 
+        
+
+@app.route('/actionpage', methods=['GET', 'POST'])
+def actionpage(): 
     if request.method == 'GET':
         return "데이터를 받아주는 페이지"
     else:
         # POST로 들어오는 데이터를 받아보자
-        search = request.form['fname']
-        print("전달된 값:" ,search)
-        return "당신이 검색한 키워드 (POST)<br>{}입니다.".format (search)
+        email = request.form['email']
+        pwd = request.form['pwd']
+        print("전달된 값:" ,email, pwd)
+
+    return "회원가입 데이터(POST)"
+            
+
+@app.route('/register')
+def register():
+    return render_template("register.html")         
 
 
 if __name__ == '__main__':
---
+++ login.py
@@ -0,0 +1,5 @@
+def login_f():
+    if email == "a@a.com" and pwd == "1234":
+        return"로그인 성공"
+    else:
+        return"로그인 실패"
